a645441401115a93fb8ece65f9eadaaa6e4cfc71
Jacob McGuire <dev.jmcguire@gmail.com>
2021-09-12 15:33:27 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div id="app">
+                <h1>Welcome</h1>
+                <h4>This is a React Application</h4>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const PATH_SOURCE = path.join(__dirname, './src');
+const PATH_DIST = path.join(__dirname, './dist');
+
+module.exports = env => {
+    console.log('Environment:', env.environment);
+
+    const environment = env.environment;
+    const isProduction = environment === 'production';
+    const isDevelopment = environment === 'development';
+
+    return {
+        mode: environment,
+        resolve: {
+            extensions: ['.js', '.ts', '.jsx', '.tsx', '.scss']
+        },
+        devServer: {
+            host: 'localhost',
+            port: 8080,
+            historyApiFallback: true,
+        },
+
+        entry: [
+            path.join(PATH_SOURCE, './index.tsx'),
+        ],
+
+        output: {
+            path: PATH_DIST,
+            filename: 'js/[name].[hash].js',
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(js|mjs|jsx|ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                    debug: false,
+                                    useBuiltIns: 'usage',
+                                    corejs: 3
+                                }],
+                                '@babel/preset-react',
+                                '@babel/preset-typescript'
+                            ]
+                        }
+                    }
+                },
+                {
+                    test: /\.module\.s(a|c)ss$/,
+                    loader: [
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                sourceMap: isDevelopment
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.s(a|c)ss$/,
+                    exclude: /\.module.(s(a|c)ss)$/,
+                    loader: [
+                        isDevelopment ? 'style-loader': MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(PATH_SOURCE, './index.html')
+            }),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+                chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+            })
+        ]
+    }
+}
